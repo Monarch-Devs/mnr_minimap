940e75c1e2ef61c9cfcc3895a4a48ac539a582ed
IlMelons <melonss2002@gmail.com>
2026-03-31 20:04:37 +0200

    Added support for TStudio Pearls Resort + Mission Row Park

--- config/client.lua
+++ config/client.lua
@@ -32,6 +32,13 @@ config.mods = {
         },
     },
     {
+        resources = { 'tstudio_missionrow_park', 'tstudio_pearls_resort' },
+        compatible = { 'atlas' },
+        mods = {
+            { id = '2_0', mod = 'tstudio6', row = 2, col = 0, alpha = 100 },
+        },
+    },
+    {
         resources = { 'tstudio_legionsquare', 'tstudio_missionrow_park', 'tstudio_pearls_resort' },
         compatible = { 'atlas' },
         mods = {
