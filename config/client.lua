3d4fab27c31cc10e1884747d50ce32dcfb67c441
IlMelons <melonss2002@gmail.com>
2026-03-31 18:18:46 +0200

    Opsie mod error

--- config/client.lua
+++ config/client.lua
@@ -56,7 +56,7 @@ config.mods = {
         resources = { 'mnr_cayo', 'tstudio_missionrow_park' },
         compatible = { 'atlas' },
         mods = {
-            { id = '2_1', mod = 'tstudio2', row = 2, col = 1, alpha = 100 },
+            { id = '2_1', mod = 'tstudio3', row = 2, col = 1, alpha = 100 },
         },
     },
 }
